0a30653883fcf2ab94af34219c5f049629e67529
Sam Blouir <scblouir+github@gmail.com>
2025-05-27 09:59:00 -0400

    Add Birdie config

--- birdie_rl/example_usage/birdie_config.py
+++ birdie_rl/example_usage/birdie_config.py
@@ -10,71 +10,71 @@
 # so this multiplier is set to 1.0.
 multipler_for_operating_on_characters_instead_of_tokens = 1.0
 
-birdie_light_config = [
+birdie_config = [
 
 
-	# # S-Denoiser
-	# {
-	# 	"name": 'prefix_language_modeling',
-	# 	# "prob": 1.0,
-	# 	"corruption_rate": 0.15,
-	# 	"prefix_fraction": 0.75,
-	# 	"paradigm_token": "[S]",
-	# },
+	# S-Denoiser
+	{
+		"name": 'prefix_language_modeling',
+		# "prob": 1.0,
+		"corruption_rate": 0.15,
+		"prefix_fraction": 0.75,
+		"paradigm_token": "[S]",
+	},
 
-	# ## R-denoisers
-	# {
-	# 	"name": 'infilling',
-	# 	# "prob": 0.5,
-	# 	"corruption_rate": 0.15,
-	# 	"paradigm_token": "[R]",
-	# 	"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 3,
-	# },
-	# {
-	# 	"name": 'infilling',
-	# 	"prob": 0.5,
-	# 	"corruption_rate": 0.15,
-	# 	"paradigm_token": "[R]",
-	# 	"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 9,
-	# },
+	## R-denoisers
+	{
+		"name": 'infilling',
+		# "prob": 0.5,
+		"corruption_rate": 0.15,
+		"paradigm_token": "[R]",
+		"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 3,
+	},
+	{
+		"name": 'infilling',
+		"prob": 0.5,
+		"corruption_rate": 0.15,
+		"paradigm_token": "[R]",
+		"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 9,
+	},
 
-	# ## X-denoisers
-	# {
-	# 	"name": 'infilling',
-	# 	# "prob": 0.25,
-	# 	"corruption_rate": 0.5,
-	# 	"paradigm_token": "[X]",
-	# 	"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 3,
-	# },
-	# {
-	# 	"name": 'infilling',
-	# 	# "prob": 0.25,
-	# 	"corruption_rate": 0.15,
-	# 	"paradigm_token": "[X]",
-	# 	"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 8,
-	# },
-	# {
-	# 	"name": 'infilling',
-	# 	# "prob": 0.25,
-	# 	"corruption_rate": 0.15,
-	# 	"paradigm_token": "[X]",
-	# 	"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 64,
-	# },
-	# {
-	# 	"name": 'infilling',
-	# 	# "prob": 0.25,
-	# 	"corruption_rate": 0.50,
-	# 	"paradigm_token": "[X]",
-	# 	"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 64,
-	# },
+	## X-denoisers
+	{
+		"name": 'infilling',
+		# "prob": 0.25,
+		"corruption_rate": 0.5,
+		"paradigm_token": "[X]",
+		"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 3,
+	},
+	{
+		"name": 'infilling',
+		# "prob": 0.25,
+		"corruption_rate": 0.15,
+		"paradigm_token": "[X]",
+		"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 8,
+	},
+	{
+		"name": 'infilling',
+		# "prob": 0.25,
+		"corruption_rate": 0.15,
+		"paradigm_token": "[X]",
+		"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 64,
+	},
+	{
+		"name": 'infilling',
+		# "prob": 0.25,
+		"corruption_rate": 0.50,
+		"paradigm_token": "[X]",
+		"mean_tokens_per_span": multipler_for_operating_on_characters_instead_of_tokens * 64,
+	},
 
 
-	# ## C-Denoiser
-	# {
-	# 	"name": 'next_token_prediction',
-	# 	# "prob": 0.6,
-	# 	"paradigm_token": "[C]",
-	# },
+	## C-Denoiser
+	{
+		"name": 'next_token_prediction',
+		# "prob": 0.6,
+		"paradigm_token": "[C]",
+	},
 
 
 	# Selective Copying
@@ -136,31 +136,31 @@ birdie_light_config = [
 		"format_style": "query_context",  # Can be "query_context" or "context_query"
 	},
 
-	# # Copying
-	# {
-	# 	"name": 'copying',
-	# 	# "prob": 0.33,  # Example probability
-	# 	"paradigm": "<|COPY|>", # Default from CopyingConfig
-	# 	"paradigm_suffix": "",   # Default from CopyingConfig
-	# },
+	# Copying
+	{
+		"name": 'copying',
+		# "prob": 0.33,  # Example probability
+		"paradigm": "<|COPY|>", # Default from CopyingConfig
+		"paradigm_suffix": "",   # Default from CopyingConfig
+	},
 
-	# # Deshuffling
-	# {
-	# 	"name": 'deshuffling',
-	# 	# "prob": 0.34,  # Example probability
-	# 	"paradigm": "<|DESHUFFLE|>", # Default from DeshufflingConfig
-	# 	"paradigm_suffix": "",       # Default from DeshufflingConfig
-	# 	"percentage_to_shuffle": 0.75, # Default from DeshufflingConfig
-	# },
+	# Deshuffling
+	{
+		"name": 'deshuffling',
+		# "prob": 0.34,  # Example probability
+		"paradigm": "<|DESHUFFLE|>", # Default from DeshufflingConfig
+		"paradigm_suffix": "",       # Default from DeshufflingConfig
+		"percentage_to_shuffle": 0.75, # Default from DeshufflingConfig
+	},
 
-	# # Deshuffling
-	# {
-	# 	"name": 'deshuffling',
-	# 	# "prob": 0.34,  # Example probability
-	# 	"paradigm": "<|DESHUFFLE|>", # Default from DeshufflingConfig
-	# 	"paradigm_suffix": "",       # Default from DeshufflingConfig
-	# 	"percentage_to_shuffle": 0.25, # Default from DeshufflingConfig
-	# },
+	# Deshuffling
+	{
+		"name": 'deshuffling',
+		# "prob": 0.34,  # Example probability
+		"paradigm": "<|DESHUFFLE|>", # Default from DeshufflingConfig
+		"paradigm_suffix": "",       # Default from DeshufflingConfig
+		"percentage_to_shuffle": 0.25, # Default from DeshufflingConfig
+	},
 
 
 ]
